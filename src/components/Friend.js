b01154868ec5e924b3178ad31b37c30417a5d9a3
abolos <andybolos@gmail.com>
2021-04-13 11:51:55 -0600

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,7 +3,7 @@ import PetsList from './PetsList'
 
 export default function Friend(props) {
   // 👉 1- What does a Friend need?
-  const {friendObj} = props
+  const {friendObj, changeCivilStatus} = props
 
   return (
     <div className='friend-friends container'>
