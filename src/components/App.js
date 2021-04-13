b8a716a364e2907813f34b1558384c695349a5b1
abolos <andybolos@gmail.com>
2021-04-13 10:45:27 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,11 @@ import friends from '../dummy-data/friends';
 export default function App() {
   // 👉 3- Initialize a slice of state to keep track of the data
   // using the dummy data as the initial value of the slice of state
+  const [myFriends, setMyFriends] = useState(friends)
 
-  // 👉 4- Initialize a slice to keep track of the value of the search box
+  // 👉 4- Initialize a slice of state to keep track of the value of the search box
   // using an empty string as the initial value of the slice
+  const [searchBox, setSearchBox] = useState('')
 
   // 👉 5- Build a `changeStatus` function that takes an id and
   // changes the `married` from true to false and viceversa
